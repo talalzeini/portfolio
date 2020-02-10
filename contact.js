748c3f6ab13b62e2dce141650ebe087a44c69270
talalzeini <56060993+talalzeini@users.noreply.github.com>
2020-02-10 15:18:16 -0800

    contact form ready and responsive

---
+++ contact.js
@@ -0,0 +1,39 @@
+console.log("it works")
+
+$(document).ready(function(){
+$('.submit').click(function(event){
+console.log('Clicked button')
+
+
+var email =$('.email').val()
+var subject =$('.subject').val()
+var message =$('.message').val()
+var statusElm =$('.status')
+statusElm.empty()
+
+if(email.length > 5 && email.includes('@') && email.includes('.')){
+    statusElm.append('')
+ } else {
+    statusElm.append('<div style="color:red; text-align:center;">The email you entered is not valid.<br><br></div>')
+    event.preventDefault()
+    }
+
+
+
+   if(message.length != 0){
+    statusElm.append('')
+} else {
+   statusElm.append('<div style="color:red; text-align:center;">Your message is empty.<br><br></div>')
+   event.preventDefault()
+   }
+
+})
+})
+
+function copyToClipboard(element) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(element).text()).select();
+    document.execCommand("copy");
+    $temp.remove();
+  }
