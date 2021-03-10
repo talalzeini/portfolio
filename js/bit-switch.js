5a679657f2400650cf4cc999d54455a9a2ad1efe
codingsensation <thecodingsensation@gmail.com>
2021-03-10 13:03:42 -0800

    removed contact page

--- js/bit-switch.js
+++
@@ -1,8 +0,0 @@
-
-
-
-
-length = 2
-myVar = Math.floor(Math.random() * (length));
-myarray = ["../images/myplane.png", "../images/parachute.jpg"]
-document.getElementById('my-img').src = myarray[myVar];
