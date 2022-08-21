3e6fd8ee13a96d9306b187f1084eca42b1dfeea9
talalzeini <talalelzeini@gmail.com>
2022-08-20 17:37:21 -0700

    refactor and fixes

--- script.js
+++ script.js
@@ -1,63 +1,76 @@
+//
+//
+//
+//
+//
+//
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+const amatyc = document.getElementById("amatyc");
+const profileImage = document.getElementById("profile-image");
+const srl = document.getElementById("srl");
+const c = document.getElementById("c");
+const pi = document.getElementById("pi");
 const dob = new Date("01/23/2001");
-function find_age(dob) {
-  let diff_ms = Date.now() - dob.getTime();
-  let age_dt = new Date(diff_ms);
-  return Math.abs(age_dt.getUTCFullYear() - 1970);
+const bitmojiPath = "images/bitmoji/";
+const age = document.getElementById("age");
+age.innerText = getAge(dob);
+//
+function getAge(dateString) {
+  var today = new Date();
+  var birthDate = new Date(dateString);
+  var age = today.getFullYear() - birthDate.getFullYear();
+  console.log(age);
+  var m = today.getMonth() - birthDate.getMonth();
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+    age--;
+  }
+  return age;
 }
 
-document.getElementById("age").innerText = find_age(dob);
-
-function wink() {
-  document.getElementById("profile-image").src = "images/bitmoji/wink.webp";
-}
-function smile() {
-  document.getElementById("profile-image").src = "images/bitmoji/profile.webp";
-}
-function scream() {
-  document.getElementById("profile-image").src = "images/bitmoji/scream.webp";
+function animateBitmoji(expression) {
+  profileImage.src = bitmojiPath + expression + ".webp";
 }
 
-function show_skills() {
-  const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-
-  const array_clone = numbers.slice();
-
+function showSkills() {
+  let arrayOfIndexes = [];
+  const arrayClone = numbers.slice();
   let len = numbers.length;
-
-  let new_array = [];
-
   for (let i = 1; i <= 9; i++) {
-    let arr = array_clone[Math.floor(Math.random() * len)];
+    let randomInteger = arrayClone[Math.floor(Math.random() * len)];
     len -= 1;
-    let index = array_clone.indexOf(arr);
-    array_clone.splice(index, 1);
-    new_array.push(arr);
+    let index = arrayClone.indexOf(randomInteger);
+    arrayClone.splice(index, 1);
+    arrayOfIndexes.push(randomInteger);
   }
 
   for (let i = 0; i < 9; i++) {
     document.getElementById("lang-" + i).src =
-      "../images/skills/" + new_array[i] + ".webp";
+      "../images/skills/" + arrayOfIndexes[i] + ".webp";
   }
 }
 
-function shuffle_skills() {
-  setTimeout(shuffle_skills, 3000);
-  show_skills();
+function activate(elements) {
+  elements[0].classList.add("active");
+  elements[1].classList.remove("hide");
 }
-document.getElementById("amatyc").classList.remove("hide");
-document.getElementById("c").classList.add("hide");
+function desactivate(elements) {
+  elements[0].classList.remove("active");
+  elements[1].classList.add("hide");
+}
+
+function shuffleSkills() {
+  setTimeout(shuffleSkills, 3000);
+  showSkills();
+}
+
 function switch_school_projects(id) {
   if (id === "srl") {
-    document.getElementById("amatyc").classList.remove("hide");
-    document.getElementById("srl").classList.add("active");
-    document.getElementById("c").classList.add("hide");
-    document.getElementById("pi").classList.remove("active");
-  } else if (id === "pi") {
-    document.getElementById("c").classList.remove("hide");
-    document.getElementById("pi").classList.add("active");
-    document.getElementById("amatyc").classList.add("hide");
-    document.getElementById("srl").classList.remove("active");
+    activate([srl, amatyc]);
+    desactivate([pi, c]);
+    return;
   }
+  desactivate([srl, amatyc]);
+  activate([pi, c]);
 }
 
-shuffle_skills();
+shuffleSkills();
